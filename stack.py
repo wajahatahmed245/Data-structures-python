c09aedbfb237927d363f039041cb83bc165bb0cd
wajahat <wajahat.ahmed245@gmail.com>
2019-03-08 14:02:55 +0500

    stack accomplished

--- stack.py
+++ stack.py
@@ -11,9 +11,20 @@ class Stack:
     def display_stack(self):
         stack = np.array(self.stack_list)
         print(stack)
+        return stack
 
     def pop(self):
-        self.stack_list.pop()
+        if not self.stack_list:
+            print("stack underflow")
+        else:
+            top = len(self.stack_list) - 1
+            removed_elements = self.stack_list[top]
+            print("The element {} is removed".format(removed_elements))
+            self.stack_list.pop()
+
+    def reverse_stack(self):
+        reversed_arr = self.display_stack()[::-1]
+        print("The reverse of stack is {}".format(reversed_arr))
 
 
 stack = Stack()
@@ -24,4 +35,12 @@ stack.push(6)
 stack.push(7)
 stack.display_stack()
 stack.pop()
+stack.pop()
+# stack.pop()
+# stack.pop()
+# stack.pop()
+# stack.pop()
+stack.push(7)
+stack.push(10)
 stack.display_stack()
+stack.reverse_stack()
