d04306f604f8844dcc21ddc453ae6b2d50e4223e
wajahat <wajahat.ahmed245@gmail.com>
2019-03-08 13:05:24 +0500

    stack development 1

--- stack.py
+++ stack.py
@@ -1,7 +1,27 @@
 import numpy as np
 
 
+class Stack:
+    def __init__(self):
+        self.stack_list = []
+
+    def push(self, element_for_stack):
+        self.stack_list.append(element_for_stack)
+
+    def display_stack(self):
+        stack = np.array(self.stack_list)
+        print(stack)
+
+    def pop(self):
+        self.stack_list.pop()
 
 
-
-
+stack = Stack()
+stack.push(3)
+stack.push(4)
+stack.push(5)
+stack.push(6)
+stack.push(7)
+stack.display_stack()
+stack.pop()
+stack.display_stack()
